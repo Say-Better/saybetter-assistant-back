15d13f3a4fff3e9b376956aded70f248ef5b71c9
zjvl324 <zjvl324@gmail.com>
2025-09-11 13:58:35 +0000

    refactor: remove controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,9 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { LoggerModule } from 'nestjs-pino';
 
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { configuration, loggerOptions } from './config';
 import { UserRepository } from './shared/user/user.repository';
-import { MemberController } from './user/controllers/user.controller';
 import { MemberCurdService } from './user/providers/user.service';
 
 @Module({
@@ -24,7 +22,6 @@ import { MemberCurdService } from './user/providers/user.service';
       inject: [ConfigService],
     }),
   ],
-  controllers: [AppController, MemberController],
   providers: [AppService, MemberCurdService, UserRepository],
 })
 export class AppModule {}
