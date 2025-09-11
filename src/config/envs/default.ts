37e6c54a2e7d6a1e5816ce4d39f05054e51c5883
zjvl324 <zjvl324@gmail.com>
2025-09-11 13:48:21 +0000

    feat: config typing

---
+++ src/config/envs/default.ts
@@ -0,0 +1,20 @@
+export const config = {
+  db: {
+    // entities: [`${__dirname}/../../entity/**/*.{js,ts}`],
+    // subscribers: [`${__dirname}/../../subscriber/**/*.{js,ts}`],
+    // migrations: [`${__dirname}/../../migration/**/*.{js,ts}`],
+  },
+  graphql: {
+    debug: true,
+    playground: {
+      settings: {
+        'request.credentials': 'include',
+      },
+    },
+    autoSchemaFile: true,
+    autoTransformHttpErrors: true,
+    // cors: { credentials: true },
+    // sortSchema: true,
+    // installSubscriptionHandlers: true,
+  },
+};
