97ac68bb9360d7f72f0134ffbcc2159010c8513c
zjvl324 <zjvl324@gmail.com>
2025-09-11 13:55:27 +0000

    refactor: GlobalModule -> ConfigModule with configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { LoggerModule } from 'nestjs-pino';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import GlobalConfigModule from './common/config/config.module';
 import { MysqlModule } from './common/db/mysql.module';
-import { loggerOptions } from './config';
+import { configuration, loggerOptions } from './config';
 import { UserRepository } from './shared/user/user.repository';
 import { MemberController } from './user/controllers/user.controller';
 import { MemberCurdService } from './user/providers/user.service';
 
 @Module({
-  imports: [LoggerModule.forRoot(loggerOptions), GlobalConfigModule, MysqlModule],
+  imports: [
+    LoggerModule.forRoot(loggerOptions),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    MysqlModule,
+  ],
   controllers: [AppController, MemberController],
   providers: [AppService, MemberCurdService, UserRepository],
 })
