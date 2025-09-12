b090ec3c44ddbc2fa984d478e2578ac37ba58668
zjvl324 <zjvl324@gmail.com>
2025-09-12 10:36:07 +0000

    feat: member save method

--- src/shared/member/member.service.ts
+++ src/shared/member/member.service.ts
@@ -1,6 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Member } from '#entity/mysql';
 
 @Injectable()
 export class UserService {
-  async save(): Promise<void> {}
+  constructor(
+    @InjectRepository(Member)
+    private readonly member: Repository<Member>,
+  ) {}
+
+  async save(): Promise<void> {
+    await this.member.save({
+      id: 'test',
+      // TODO: password encrypt
+      password: 'test',
+      preferSubject: 'test',
+      name: 'test',
+      age: 'test',
+      gender: 0,
+    });
+  }
 }
